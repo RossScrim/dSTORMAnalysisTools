a9f2e7784235e7d69689bd731aa05eeaeb5866c2
RossScrim <70106868+RossScrim@users.noreply.github.com>
2024-03-20 17:09:57 +0000

    Implementing a batch and preview mode

--- shared_resources/file_management.py
+++ shared_resources/file_management.py
@@ -3,17 +3,41 @@ import numpy as np
 import errno
 import os
 
-def get_files(folder_path):
-    """
-    This function retrieves a list of filenames within a specified folder.
 
-     rgs:
+def get_filename(filepath):
+    filepath, _ = os.path.splitext(filepath)
+    filename = filepath.split("\\")
+    return filename[-1]
+
+
+def get_file_extension(filepath):
+    _, file_extension = os.path.splitext(filepath)
+    return file_extension
+
+
+def generate_full_savename(filepath:str, savepath:str, save_label: str, new_file_format) -> str:
+    filename = get_filename(filepath)
+    savename =  f"{savepath}" + "\\" + f"{filename}_" + f"{save_label}{new_file_format}" 
+    i = 1
+    while os.path.exists(savename):
+        savename = f"{savename[:-len(new_file_format)]}_{i}{new_file_format}"  # Add number before extension
+        i += 1 
+    return savename
+        
+
+def get_full_filespaths(folder_path):
+    """
+    This function retrieves a list of of the fullfil within a folder set for batch processing.
+
+    rgs:
       folder_path (str): Path to the folder containing the files.
 
     Returns:
-    list: List of filenames within the folder.
+    list: List of full file paths within the folder.
     """
-    return os.listdir(folder_path)
+    filenames = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    full_paths = [os.path.join(folder_path, filename) for filename in filenames]
+    return full_paths
 
 
 def read_localisation_HDF5(filepath: str) -> pd.DataFrame:
@@ -42,7 +66,7 @@ def read_localisation_csvdata(filepath: str) -> pd.DataFrame:
     return pd.read_csv(filepath)
 
 
-def read_files(file_path: str, format="csv") -> pd.DataFrame:
+def read_files(filepath: str) -> pd.DataFrame:
     """
     Reads localization data based on the specified format and file path.
 
@@ -55,13 +79,19 @@ def read_files(file_path: str, format="csv") -> pd.DataFrame:
 
     Raises:
         ValueError: If the provided format is not supported.
+
     """
-    if format == "csv":
-        file = read_localisation_csvdata(file_path)
-    elif format == "hdf5":
-        file = read_localisation_HDF5(file_path)
-    else:
+    format = get_file_extension(filepath)
+    try:
+     if format == ".csv":
+        file = read_localisation_csvdata(filepath)
+     elif format == ".hdf5":
+        file = read_localisation_HDF5(filepath)
+     else:
         raise ValueError("Unsupported data format:", format)
+    except ValueError as e:  # Catch only ValueError (format errors)
+        print(f"Skipping file '{filepath}': Unsupported format ({e})")
+        file = None  # Set file to None to indicate failure
     return file
 
 
